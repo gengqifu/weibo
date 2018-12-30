533170e259cf0918aa8b74fb93c7ca4f02d92c58
gengqifu <gengqifu@guazi.com>
2018-12-30 17:56:21 +0800

    添加图标

---
+++ weibo/weibo/LYTabBarController.h
@@ -0,0 +1,17 @@
+//
+//  LYTabBarController.h
+//  weibo
+//
+//  Created by 耿启富 on 2018/12/30.
+//  Copyright © 2018年 sirius.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface LYTabBarController : UITabBarController
+
+@end
+
+NS_ASSUME_NONNULL_END
